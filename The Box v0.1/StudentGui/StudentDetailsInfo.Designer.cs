af0f60c71190f8a999d5208e8f79c19993dfbc7c
Mostafa M. Gad <62313686+mostafagad464@users.noreply.github.com>
2022-03-10 00:15:26 +0200

    Revert "master"
    This reverts commit 8d350d27a0fbd92b5cd8f5a51001c5eb07244b35.

---
+++ The Box v0.1/StudentGui/StudentDetailsInfo.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace The_Box_v0._1.Forms
+{
+    partial class StudentDetailsInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImagePanel = new System.Windows.Forms.Panel();
+            this.MainImage = new System.Windows.Forms.PictureBox();
+            this.UserName = new System.Windows.Forms.Label();
+            this.UserNameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.IdTraclLabel = new System.Windows.Forms.Label();
+            this.LeaderId = new System.Windows.Forms.Label();
+            this.LeaderIdT = new System.Windows.Forms.Label();
+            this.TrackIdT = new System.Windows.Forms.Label();
+            this.EmailT = new System.Windows.Forms.Label();
+            this.AgeT = new System.Windows.Forms.Label();
+            this.NameT = new System.Windows.Forms.Label();
+            this.UserNameT = new System.Windows.Forms.Label();
+            this.ImagePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagePanel
+            // 
+            this.ImagePanel.Controls.Add(this.LeaderIdT);
+            this.ImagePanel.Controls.Add(this.TrackIdT);
+            this.ImagePanel.Controls.Add(this.EmailT);
+            this.ImagePanel.Controls.Add(this.AgeT);
+            this.ImagePanel.Controls.Add(this.NameT);
+            this.ImagePanel.Controls.Add(this.UserNameT);
+            this.ImagePanel.Controls.Add(this.LeaderId);
+            this.ImagePanel.Controls.Add(this.IdTraclLabel);
+            this.ImagePanel.Controls.Add(this.EmailLabel);
+            this.ImagePanel.Controls.Add(this.AgeLabel);
+            this.ImagePanel.Controls.Add(this.NameLabel);
+            this.ImagePanel.Controls.Add(this.UserNameLabel);
+            this.ImagePanel.Controls.Add(this.pictureBox2);
+            this.ImagePanel.Controls.Add(this.label1);
+            this.ImagePanel.Controls.Add(this.textBox1);
+            this.ImagePanel.Controls.Add(this.pictureBox1);
+            this.ImagePanel.Controls.Add(this.UserName);
+            this.ImagePanel.Controls.Add(this.UserNameText);
+            this.ImagePanel.Controls.Add(this.MainImage);
+            this.ImagePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ImagePanel.Location = new System.Drawing.Point(0, 0);
+            this.ImagePanel.Name = "ImagePanel";
+            this.ImagePanel.Size = new System.Drawing.Size(800, 344);
+            this.ImagePanel.TabIndex = 0;
+            // 
+            // MainImage
+            // 
+            this.MainImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainImage.Location = new System.Drawing.Point(0, 0);
+            this.MainImage.Name = "MainImage";
+            this.MainImage.Size = new System.Drawing.Size(800, 344);
+            this.MainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MainImage.TabIndex = 0;
+            this.MainImage.TabStop = false;
+            this.MainImage.Click += new System.EventHandler(this.MainImage_Click);
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.BackColor = System.Drawing.Color.Transparent;
+            this.UserName.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.UserName.ForeColor = System.Drawing.Color.White;
+            this.UserName.Location = new System.Drawing.Point(163, 73);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(107, 23);
+            this.UserName.TabIndex = 22;
+            this.UserName.Text = "User Name :";
+            // 
+            // UserNameText
+            // 
+            this.UserNameText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.UserNameText.Font = new System.Drawing.Font("Trebuchet MS", 12F);
+            this.UserNameText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(53)))), ((int)(((byte)(82)))));
+            this.UserNameText.Location = new System.Drawing.Point(301, 56);
+            this.UserNameText.Margin = new System.Windows.Forms.Padding(2);
+            this.UserNameText.Multiline = true;
+            this.UserNameText.Name = "UserNameText";
+            this.UserNameText.Size = new System.Drawing.Size(204, 40);
+            this.UserNameText.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(163, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 23);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "User Name :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 12F);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(53)))), ((int)(((byte)(82)))));
+            this.textBox1.Location = new System.Drawing.Point(301, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 40);
+            this.textBox1.TabIndex = 24;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 344);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 344);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 26;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.UserNameLabel.ForeColor = System.Drawing.Color.White;
+            this.UserNameLabel.Location = new System.Drawing.Point(143, 84);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(96, 23);
+            this.UserNameLabel.TabIndex = 27;
+            this.UserNameLabel.Text = "User Name";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.NameLabel.ForeColor = System.Drawing.Color.White;
+            this.NameLabel.Location = new System.Drawing.Point(143, 128);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(53, 23);
+            this.NameLabel.TabIndex = 28;
+            this.NameLabel.Text = "Label";
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.AgeLabel.ForeColor = System.Drawing.Color.White;
+            this.AgeLabel.Location = new System.Drawing.Point(143, 166);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(40, 23);
+            this.AgeLabel.TabIndex = 29;
+            this.AgeLabel.Text = "Age";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.EmailLabel.ForeColor = System.Drawing.Color.White;
+            this.EmailLabel.Location = new System.Drawing.Point(143, 209);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(54, 23);
+            this.EmailLabel.TabIndex = 30;
+            this.EmailLabel.Text = "Email";
+            // 
+            // IdTraclLabel
+            // 
+            this.IdTraclLabel.AutoSize = true;
+            this.IdTraclLabel.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.IdTraclLabel.ForeColor = System.Drawing.Color.White;
+            this.IdTraclLabel.Location = new System.Drawing.Point(144, 252);
+            this.IdTraclLabel.Name = "IdTraclLabel";
+            this.IdTraclLabel.Size = new System.Drawing.Size(71, 23);
+            this.IdTraclLabel.TabIndex = 31;
+            this.IdTraclLabel.Text = "Track Id";
+            // 
+            // LeaderId
+            // 
+            this.LeaderId.AutoSize = true;
+            this.LeaderId.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.LeaderId.ForeColor = System.Drawing.Color.White;
+            this.LeaderId.Location = new System.Drawing.Point(144, 285);
+            this.LeaderId.Name = "LeaderId";
+            this.LeaderId.Size = new System.Drawing.Size(85, 23);
+            this.LeaderId.TabIndex = 32;
+            this.LeaderId.Text = "Leader Id";
+            // 
+            // LeaderIdT
+            // 
+            this.LeaderIdT.AutoSize = true;
+            this.LeaderIdT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.LeaderIdT.ForeColor = System.Drawing.Color.White;
+            this.LeaderIdT.Location = new System.Drawing.Point(309, 285);
+            this.LeaderIdT.Name = "LeaderIdT";
+            this.LeaderIdT.Size = new System.Drawing.Size(85, 23);
+            this.LeaderIdT.TabIndex = 38;
+            this.LeaderIdT.Text = "Leader Id";
+            // 
+            // TrackIdT
+            // 
+            this.TrackIdT.AutoSize = true;
+            this.TrackIdT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.TrackIdT.ForeColor = System.Drawing.Color.White;
+            this.TrackIdT.Location = new System.Drawing.Point(309, 252);
+            this.TrackIdT.Name = "TrackIdT";
+            this.TrackIdT.Size = new System.Drawing.Size(71, 23);
+            this.TrackIdT.TabIndex = 37;
+            this.TrackIdT.Text = "Track Id";
+            // 
+            // EmailT
+            // 
+            this.EmailT.AutoSize = true;
+            this.EmailT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.EmailT.ForeColor = System.Drawing.Color.White;
+            this.EmailT.Location = new System.Drawing.Point(308, 209);
+            this.EmailT.Name = "EmailT";
+            this.EmailT.Size = new System.Drawing.Size(54, 23);
+            this.EmailT.TabIndex = 36;
+            this.EmailT.Text = "Email";
+            // 
+            // AgeT
+            // 
+            this.AgeT.AutoSize = true;
+            this.AgeT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.AgeT.ForeColor = System.Drawing.Color.White;
+            this.AgeT.Location = new System.Drawing.Point(308, 166);
+            this.AgeT.Name = "AgeT";
+            this.AgeT.Size = new System.Drawing.Size(40, 23);
+            this.AgeT.TabIndex = 35;
+            this.AgeT.Text = "Age";
+            // 
+            // NameT
+            // 
+            this.NameT.AutoSize = true;
+            this.NameT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.NameT.ForeColor = System.Drawing.Color.White;
+            this.NameT.Location = new System.Drawing.Point(308, 128);
+            this.NameT.Name = "NameT";
+            this.NameT.Size = new System.Drawing.Size(53, 23);
+            this.NameT.TabIndex = 34;
+            this.NameT.Text = "Label";
+            // 
+            // UserNameT
+            // 
+            this.UserNameT.AutoSize = true;
+            this.UserNameT.Font = new System.Drawing.Font("Trebuchet MS", 12.25F, System.Drawing.FontStyle.Bold);
+            this.UserNameT.ForeColor = System.Drawing.Color.White;
+            this.UserNameT.Location = new System.Drawing.Point(308, 84);
+            this.UserNameT.Name = "UserNameT";
+            this.UserNameT.Size = new System.Drawing.Size(96, 23);
+            this.UserNameT.TabIndex = 33;
+            this.UserNameT.Text = "User Name";
+            // 
+            // ImageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ImagePanel);
+            this.Name = "ImageForm";
+            this.Text = "ImageForm";
+            this.ImagePanel.ResumeLayout(false);
+            this.ImagePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ImagePanel;
+        private System.Windows.Forms.PictureBox MainImage;
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.TextBox UserNameText;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LeaderId;
+        private System.Windows.Forms.Label IdTraclLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.Label LeaderIdT;
+        private System.Windows.Forms.Label TrackIdT;
+        private System.Windows.Forms.Label EmailT;
+        private System.Windows.Forms.Label AgeT;
+        private System.Windows.Forms.Label NameT;
+        private System.Windows.Forms.Label UserNameT;
+    }
+}
